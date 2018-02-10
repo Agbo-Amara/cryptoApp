926b01bc4aceebcc658af0bea66a90e3cbe60d88
amarachukwu-agbo <amarachukwu.agbo@gmail.com>
2018-02-10 14:12:47 +0100

    CryptoApp

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cryptoApp
 A simple command-line app that provides the currrent exchange rate of any cryptocurrency in any currency 
+## Running Locally
+Make sure you have Node.js installed
+git clone https://github.com/Agbo-Amara/cryptoApp.git
+cd cryptoApp
+npm install
+node app
--- app.js
+++ app.js
@@ -1,37 +1,61 @@
-// Load in the http module
+// Load in the https module
 const https = require('https');
+
+// Load in the yargs module
 const argv = require('yargs').argv;
+
+// Load in the colors-cli module
 const color = require('colors-cli/safe');
 
-const cryptoSymbol = argv.cryptoSymbol || 'BTC,ETH,XRP,LTC';
-const currency = argv.currency || 'USD,EUR,GBP,NGN';
+// Accept arguments from yargs or use the default values
+const cryptoSymbols = argv.cryptoSymbols || 'BTC,ETH,XRP,LTC';
+const currencies = argv.currencies || 'USD,EUR,GBP,NGN';
+
+// Set the options object
 const options = {
     host: 'min-api.cryptocompare.com',
-    path: `/data/pricemulti?fsyms=${ cryptoSymbol }&tsyms=${ currency }`
+    path: `/data/pricemulti?fsyms=${ cryptoSymbols }&tsyms=${ currencies }`
 };
+
+// Set color for error messages
 const errorColor = color.red.bold;
+
+// Set color for the welcome message and cryptoSymbols
 const appColor = color.yellow.bold;
-const starColor = color.cyan;
 
-console.log('Welcome to the cryptoApp !!! \n');
+// Set color for our fancy line
+const lineColor = color.cyan;
 
+console.log(appColor('Welcome to the cryptoApp !!! \n'));
+
+// Make the GET request to the API
 https.get(options, (res) => {
     let body = '';
+
+    // set the encoding of the response object to utf-8
     res.setEncoding('utf8');
+
     res.on('data', (data) => {
         body += data;
     });
     res.on('end', () => {
         try {
-            const rate = JSON.parse(body);
-            //console.log(rate);
-            const symbols = cryptoSymbol.split(',');
+            // Convert the response to JSON
+            const output = JSON.parse(body);
+
+            // Convert crytoSymbols from a string to an array
+            const symbols = cryptoSymbols.split(',');
+
             console.log('Current cryptocurrency exchange rates\n');
-            if (rate.Message) return console.log(errorColor(`${rate.Response}:${rate.Message}`));
+
+            // Return error message if there is an error with the crytocurrencies or currencies
+            if (output.Message) return console.log(errorColor(`${output.Response}:${output.Message}`));
+            
+            // Print each cryptocurrencies exchange value from the output
             symbols.map(symbol => {
                     console.log(appColor(`${symbol}`));
-                    console.log(rate[`${ symbol }`]);
-                    console.log(starColor('***************************************************************'));    
+                    console.log(output[`${ symbol }`]);
+                    console.log(lineColor('***************************************************************'));    
             });
         }
         catch(err) {
